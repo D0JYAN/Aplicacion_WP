bd04ddf3e4cad2f5d0d73b5127dcbdec6c2e29cd
JIELSAN <jesussjg811@gmail.com>
2024-09-29 18:37:22 -0600

    correccion del front

--- navbar.php
+++ navbar.php
@@ -1,31 +1,29 @@
-<div class="container-fluid">
-    <nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">
-                <img src="img/icon_logo.png" alt="Logo" width="125" height="30">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="personajes.php">Personajes</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="infectados.php">Infectados</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="armas.php">Armas</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="campañas.php">Campañas</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="items.php">Items</a>
-                    </li>
-                </ul>
-            </div>
+<nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">
+            <img src="img/icon_logo.png" alt="Logo" width="125" height="30">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="personajes.php">Personajes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="infectados.php">Infectados</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="armas.php">Armas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="campañas.php">Campañas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="items.php">Items</a>
+                </li>
+            </ul>
         </div>
-    </nav>
-</div>
+    </div>
+</nav>
