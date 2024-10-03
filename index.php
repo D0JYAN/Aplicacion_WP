6ad5c195f1841bcc05f106f147fe1deab46cb95e
JIELSAN <jesussjg811@gmail.com>
2024-10-03 14:57:35 -0600

    script del serviceworker en index

--- index.php
+++ index.php
@@ -63,8 +63,8 @@
                     <img src="img/img_002.webp" class="card-img-top">
                     <div class="card-body">
                         <p class="card-text">
-                            Valve (los creadores de Counter-Strike, Half-Life y otros) presenta Left 4 Dead, una nueva aventura cooperativa de acción y 
-                            terror para PC y Xbox 360, que coloca a cuatro jugadores en una épica batalla por la supervivencia contra aterradoras hordas de zombis y monstruos 
+                            Valve (los creadores de Counter-Strike, Half-Life y otros) presenta Left 4 Dead, una nueva aventura cooperativa de acción y
+                            terror para PC y Xbox 360, que coloca a cuatro jugadores en una épica batalla por la supervivencia contra aterradoras hordas de zombis y monstruos
                             mutantes.
                         </p>
                         <a href="https://store.steampowered.com/app/500/Left_4_Dead/" class="btn btn-success">Donde Comprar</a>
@@ -76,8 +76,8 @@
                     <img src="img/img_003.webp" class="card-img-top">
                     <div class="card-body">
                         <p class="card-text">
-                        Ambientado en el apocalipsis zombi, Left 4 Dead 2 (L4D2) es la esperadísima secuela del galardonado Left 4 Dead, el juego cooperativo número 1 de 2008. 
-                        Este FPS cooperativo de acción terrorífica lleva a tus amigos y a ti a través de ciudades, pantanos y cementerios del Profundo Sur de EE. UU.
+                            Ambientado en el apocalipsis zombi, Left 4 Dead 2 (L4D2) es la esperadísima secuela del galardonado Left 4 Dead, el juego cooperativo número 1 de 2008.
+                            Este FPS cooperativo de acción terrorífica lleva a tus amigos y a ti a través de ciudades, pantanos y cementerios del Profundo Sur de EE. UU.
                         </p>
                         <a href="https://store.steampowered.com/app/550/Left_4_Dead_2/" class="btn btn-success">Donde Comprar</a>
                     </div>
@@ -92,3 +92,22 @@
 </body>
 
 </html>
+
+<script>
+    window.addEventListener('load', () => {
+        registerSW();
+    });
+
+    // Register the Service Worker
+    async function registerSW() {
+        if ('serviceWorker' in navigator) {
+            try {
+                await navigator
+                    .serviceWorker
+                    .register('serviceworker.js');
+            } catch (e) {
+                console.log('Error en el registro del SW');
+            }
+        }
+    }
+</script>
