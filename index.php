c98500a9cd2726b32c5cc06094bbac3862200d0b
JIELSAN <jesussjg811@gmail.com>
2024-10-03 17:47:47 -0600

    correccion de la ubicacion del script del serviceworker

--- index.php
+++ index.php
@@ -89,25 +89,25 @@
     <?php
     include("footer.php");
     ?>
+
+    <script>
+        window.addEventListener('load', () => {
+            registerSW();
+        });
+
+        // Register the Service Worker
+        async function registerSW() {
+            if ('serviceWorker' in navigator) {
+                try {
+                    await navigator
+                        .serviceWorker
+                        .register('serviceworker.js');
+                } catch (e) {
+                    console.log('Error en el registro del SW');
+                }
+            }
+        }
+    </script>
 </body>
 
 </html>
-
-<script>
-    window.addEventListener('load', () => {
-        registerSW();
-    });
-
-    // Register the Service Worker
-    async function registerSW() {
-        if ('serviceWorker' in navigator) {
-            try {
-                await navigator
-                    .serviceWorker
-                    .register('serviceworker.js');
-            } catch (e) {
-                console.log('Error en el registro del SW');
-            }
-        }
-    }
-</script>
