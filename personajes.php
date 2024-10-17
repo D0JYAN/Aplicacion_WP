ea4a66e8737dad58e8b4cd57be2e5cf50dacb805
JIELSAN <jesussjg811@gmail.com>
2024-10-17 11:31:15 -0600

    modificacion en seccion de personajes (imagenes, texto y estrutura de la pagina)

--- personajes.php
+++ personajes.php
@@ -26,7 +26,201 @@
     ?>
 
     <div class="container-fluid">
-        <h1>hola</h1>
+
+        <div class="row">
+
+            <div class="col">
+                <div class="card mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="img/bill.webp" class="img-fluid rounded-start" alt="..." style="width: 250px; height: 320px;">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Bill</h5>
+                                <p class="card-text">
+                                    William "Bill" Overbeck es uno de los cuatro Supervivientes de Left 4 Dead. Él era un veterano de la Guerra de Vietnam que sirvió
+                                    en el Primer Grupo de Operaciones Especiales del Ejército de los Estados Unidos.
+                                </p>
+                                <p class="card-text">
+                                    Bill muere antes de los acontecimientos de Defunción de Left 4 Dead 2. En la campaña El Sacrificio el se sacrificó para levantar
+                                    el puente para que los otros tres Supervivientes pudieran salvarse de la horda.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="img/francis.webp" class="img-fluid rounded-start" alt="..." style="width: 250px; height: 320px;">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Francis</h5>
+                                <p class="card-text">
+                                    Francis es uno de los cuatro Supervivientes de Left 4 Dead. Él es un rudo motociclista cubierto de tatuajes en su cuello y sus brazos.
+                                    Sus tatuajes lo identifican como miembro de la "Legión del infierno". Francis, junto con Zoey y Louis, aparece en Left 4 Dead 2 en la
+                                    campaña Defunción donde conocen a los Supervivientes de Left 4 Dead 2.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row">
+
+            <div class="col">
+                <div class="card mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="img/luis.webp" class="img-fluid rounded-start" alt="..." style="width: 250px; height: 320px;">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Luis</h5>
+                                <p class="card-text">
+                                    Louis es uno de los cuatro Supervivientes de Left 4 Dead. Louis solía ser un analista de sistemas en "Franklin Brothers" en el
+                                    departamento de Informática con sede en Filadelfia, hasta el segundo día después de la primera infección. Louis, junto con Zoey y
+                                    Francis, aparecen en Left 4 Dead 2 en la campaña Defunción donde conocen a los Supervivientes de Left 4 Dead 2.
+                                </p>
+                                <p class="card-text">
+                                    Louis es el Superviviente más optimista y trata de mantener el alto estima del grupo, no importa cuál sea la situación.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="img/zoey.webp" class="img-fluid rounded-start" alt="..." style="width: 250px; height: 320px;">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Zoey</h5>
+                                <p class="card-text">
+                                    Zoey es uno de los cuatro Supervivientes de Left 4 Dead. En general, Zoey es quizás la más amable de los Supervivientes, y la que es
+                                    tratada de la manera más amable siendo la menor del grupo en Left 4 Dead. Zoey, junto con Louis y Francis, aparece en Left 4 Dead 2 en
+                                    la campaña Defunción donde conocen a los Supervivientes de Left 4 Dead 2. Su personalidad se muestra diferente a en Left 4 Dead, se le
+                                    ve más agresiva y, a veces, enojada. Esto es probablemente debido al hecho de que quedo muy impactada por la muerte de Bill.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row">
+
+            <div class="col">
+                <div class="card mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="img/coach.webp" class="img-fluid rounded-start" alt="..." style="width: 250px; height: 320px;">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Coach</h5>
+                                <p class="card-text">
+                                    Es un hombre de gran corazón y mayor apetito que comenzó una carrera como defensa en su equipo de la universidad, pero una lesión en
+                                    la rodilla le obligó a apartarse de ese futuro y acabó como profesor de educación física en una escuela secundaria en su ciudad natal.
+                                    Tras ver como su querida ciudad ha sido devastada por la infección, no tiene más que usar sus conocimientos para guiar a los
+                                    supervivientes por los enrevesados caminos hacia Nueva Orleans.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="img/ellis.webp" class="img-fluid rounded-start" alt="..." style="width: 250px; height: 320px;">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Ellis</h5>
+                                <p class="card-text">
+                                    Ellis es el miembro más joven y entusiasta de su grupo de supervivientes, siendo originario de la ciudad de Savannah al igual de Coach.
+                                    Es bastante optimista y amigable con el resto de sus compañeros, algo que se demuestra con sus líneas de voz durante el juego.
+                                    Lo más característico de él son las historias que cuenta durante las campañas sobre su amigo Keith en un intento por agradar a sus
+                                    nuevos amigos. También es fanático de la banda de los Midnight Riders y el corredor de coches Jimmy Gibbs Jr, a quién considera un
+                                    ídolo americano. Lleva puesto su overol de trabajo amarrado a la cintura, una playera color amarillo con un logotipo en negro y la
+                                    gorra del taller en que trabajaba.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row">
+
+            <div class="col">
+                <div class="card mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="img/nick.webp" class="img-fluid rounded-start" alt="..." style="width: 250px; height: 320px;">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Nick</h5>
+                                <p class="card-text">
+                                    Es un lúdopata "adicto a los juegos" que estaba detenido, arrestado por fraude. Su vida lo ha llevado a aprender una gran lección:
+                                    no confiar en nadie, pues gracias a esto lo atraparon. Llega a Savannah para su juicio pero se encuentra con cadáveres amontonados e
+                                    infectados, lo primero que se le ocurre es entrar a una tienda y robarse el traje más caro "de 3000 dolares y de preferencia blanco",
+                                    después de vestirse, se da cuenta de que no esta solo, y se encuentra con otros 3 supervivientes, lo que lo llevará a forjar una débil
+                                    alianza con ellos lo que lo hará a confiar cada vez más en lo que queda de la humanidad.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="img/rochell.webp" class="img-fluid rounded-start" alt="..." style="width: 250px; height: 320px;">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Rochell</h5>
+                                <p class="card-text">
+                                    Es una asistente de producción en un canal de televisión, es originaria de Cleveland, Ohio.
+                                </p>
+                                <p class="card-text">
+                                    Su trabajo consistía en acarrear los equipos técnicos y preparar el café hasta que la dirección del canal le informó que debía ir a
+                                    Savannah para colaborar en la realización de un reportaje sobre la evacuación que se está haciendo en dicha ciudad. Mientras preparaba
+                                    las cámaras, se ve envuelta en una lucha por la supervivencia. Aún en medio de la infección, los zombis no detendrán sus sueños y ahora
+                                    pondrá a prueba toda su inteligencia y sus deseos de seguir viviendo para poder llevar a un grupo de desconocidos, y a ella misma,
+                                    fuera de este infierno.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
     </div>
 
     <?php
